2a56237df33259154fc3017c98179ac3ec318de2
senamjempol <48201831+senamjempol@users.noreply.github.com>
2022-09-11 15:54:37 +0700

    Update appbrowser.js

--- appbrowser.js
+++ appbrowser.js
@@ -1,15 +1,1 @@
-$( document ).ready(function() {
-var requestUrl = "http://ip-api.com/json";
 
-    $.ajax({
-      url: requestUrl,
-      type: 'GET',
-      success: function(json)
-      {
-        $('#recomended').html(json.country);
-      },
-      error: function(err)
-      {}
-    });
-    
-});
