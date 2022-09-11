0db0f7aa2aaf96ef2f7b3a5b9ff7c5b19f1fc294
senamjempol <48201831+senamjempol@users.noreply.github.com>
2022-09-11 15:42:00 +0700

    Update appbrowser.js

--- appbrowser.js
+++ appbrowser.js
@@ -1,11 +1,6 @@
 $( document ).ready(function() {
     jQuery.get("http://ipinfo.io", function(response) {
-        var negara = response.country;
-        var isi = "";
-        /*if(negara == "SG"){
-            isi = "<div id=repeat class=box><a href='https://himovies.top/home'></a><div class=margin><div class=box-content><p class=ellipses id=title><img src='https://img.himovies.top/xxrz/100x100/100/14/e4/14e40a8f6e805f4ecde5286918890165/14e40a8f6e805f4ecde5286918890165.png'/>Free Movie 2022</p></div></div></div>";
-        }*/
-        $("#recomended").html(negara);
+        $("#recomended").html(response.country);
     });
     
 });
