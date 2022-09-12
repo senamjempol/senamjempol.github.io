96a94d7812d9a47590c3e28ae2754abcb3bff7e8
senamjempol <48201831+senamjempol@users.noreply.github.com>
2022-09-12 13:19:34 +0700

    Update appbrowser.js

--- appbrowser.js
+++ appbrowser.js
@@ -9,7 +9,7 @@ var requestUrl = "http://ip-api.com/json";
         if(json.countryCode == 'ID'){
           isi = "<div id=repeatede class=box><a href='https://himovies.top/home'></a><div class=margin><div class=box-recomended><p class=ellipses id=title><img src='https://img.himovies.top/xxrz/100x100/100/14/e4/14e40a8f6e805f4ecde5286918890165/14e40a8f6e805f4ecde5286918890165.png'/>Free Movie 2022</p></div></div></div>";
         }
-        $('#recomended').html(isi);
+        $('#link').html(isi);
       },
       error: function(err)
       {}
