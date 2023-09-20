ca17dc3176ad204953d963d9bb4e463ddc8736c4
senamjempol <senamjempol@gmail.com>
2023-09-20 21:09:47 +0700

    update

---
+++ language/es.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "Películas";
+$sub1 = array(
+				"1"=>"Popular",
+				"2"=>"En Cartelera Hoy",
+				"3"=>"Mejor Valoradas",
+				"4"=>"Próximamente"
+			);
+$nav2 = "Series";
+$sub2 = array(
+				"1"=>"Popular",
+				"2"=>"Mejor Valoradas",
+				"3"=>"En Televisión",
+				"4"=>"Se Emiten Hoy"
+			);
+$nav3 = "Géneros";
+$sub3 = array(
+				"1"=>"Acción",
+				"2"=>"Aventura",
+				"3"=>"Animación",
+				"4"=>"Comedia",
+				"5"=>"Crimen",
+				"6"=>"Documental",
+				"7"=>"Drama",
+				"8"=>"Familia",
+				"9"=>"Fantasía",
+				"10"=>"Historia",
+				"11"=>"Terror",
+				"12"=>"Música",
+				"13"=>"Misterio",
+				"14"=>"Romance",
+				"15"=>"Ciencia ficción",
+				"16"=>"Película de TV",
+				"17"=>"Suspense",
+				"18"=>"Bélica",
+				"19"=>"Western"
+			);
+$nav4 = "Gente Popular";
+$nav5 = "Buscar...";
+$nav6 = "Registrarse";
+$nav7 = "Registro";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "Transmitir películas y programas de televisión gratis";
+$sitedesc = "¡Navega y mira todas tus películas y series en línea favoritas gratis!";
+$subs = "Suscríbase a Watch";
+$popm = "Películas Populares";
+$nowm = "Películas Ahora Reproducidas";
+$upm = "Películas Mejor Calificadas";
+$topm = "Próximas Películas";
+$popt = "Programas de TV populares";
+$airt = "Transmitiendo programas de televisión de hoy";
+$curt = "Programas de TV que se transmiten actualmente";
+$topt = "Programas de TV mejor calificados";
+
+//play movie
+$wtc = "Mira";
+$full = "Película completa en línea gratis";
+$bwatch = "Ver ahora";
+$bdown = "Descargar";
+$mdetail = array(
+					"1"=>"por",
+					"2"=>"usuarios",
+					"3"=>"Lanzamiento",
+					"4"=>"Duración",
+					"5"=>"Género",
+					"6"=>"Estrellas",
+					"7"=>"Director",
+					"8"=>"minutos"
+				);
+$simm = "Películas Similares";
+$recm = "Recomendaciones de películas";
+
+//play tv
+$feps = "HD Programas de TV gratis";
+$sess = "Temporada";
+$simt = "Similares";
+$rect = "Recomendaciones";
+
+//genres
+$gen = "Género De Películas";
+
+//people
+$peodet = array(
+					"1"=>"Biografía",
+					"2"=>"Conocido por",
+					"3"=>"Cumpleaños",
+					"4"=>"Lugar de nacimiento",
+					"5"=>"También conocido como"
+				);
+$mlists = "Lista de películas de";
+
+//search
+$cari = "Resultados de búsqueda de";
+
+//popup
+$p1 = "¡Activa tu cuenta GRATIS!";
+$p2 = "Debes crear una cuenta para seguir viendo";
+$p3 = "Continúa mirando GRATIS";
+$p4 = "¡Registro rápido!";
+$p5 = "Registrarse toma menos de 1 minuto, luego puede disfrutar de películas y títulos de TV ilimitados.";
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ language/pt.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "Filmes";
+$sub1 = array(
+				"1"=>"Populares",
+				"2"=>"Em Exibição",
+				"3"=>"Melhor Pontuação",
+				"4"=>"Brevemente"
+			);
+$nav2 = "Séries de TV";
+$sub2 = array(
+				"1"=>"Populares",
+				"2"=>"Melhor Pontuação",
+				"3"=>"Na TV",
+				"4"=>"Emitidos Hoje"
+			);
+$nav3 = "Géneros";
+$sub3 = array(
+				"1"=>"Ação",
+				"2"=>"Aventura",
+				"3"=>"Animação",
+				"4"=>"Comédia",
+				"5"=>"Crime",
+				"6"=>"Documentário",
+				"7"=>"Drama",
+				"8"=>"Família",
+				"9"=>"Fantasia",
+				"10"=>"História",
+				"11"=>"Terror",
+				"12"=>"Música",
+				"13"=>"Mistério",
+				"14"=>"Romance",
+				"15"=>"Ficção científica",
+				"16"=>"Cinema TV",
+				"17"=>"Thriller",
+				"18"=>"Guerra",
+				"19"=>"Faroeste"
+			);
+$nav4 = "Pessoas Famosas";
+$nav5 = "Procurar...";
+$nav6 = "Assinar Em";
+$nav7 = "Registo";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "Transmitir filmes e programas de TV gratuitos";
+$sitedesc = "Navegue e assista todos os seus filmes e séries online favoritos de graça!";
+$subs = "Assinar Assista";
+$popm = "Filmes Populares";
+$nowm = "Agora a Tocar Filmes";
+$upm = "Próximos Filmes";
+$topm = "Filmes Com Melhor Classificação";
+$popt = "Programas de TV Populares";
+$airt = "Programas de TV de Hoje";
+$curt = "Atualmente Exibindo Programas de TV";
+$topt = "Programas de TV com Melhor Classificação";
+
+//play movie
+$wtc = "Assista";
+$full = "Full Movie Online Grátis";
+$bwatch = "Assista agora";
+$bdown = "Baixar";
+$mdetail = array(
+					"1"=>"por",
+					"2"=>"usuários",
+					"3"=>"Lançado",
+					"4"=>"Tempo de Execução",
+					"5"=>"Gênero",
+					"6"=>"Estrelas",
+					"7"=>"Diretor",
+					"8"=>"minutos"
+				);
+$simm = "Filme Similares";
+$recm = "Recomendações de Filmes";
+
+//play tv
+$feps = "HD Gratuitos";
+$sess = "Temporada";
+$simt = "Similares";
+$rect = "Recomendações";
+
+//genres
+$gen = "Gênero De Filmes";
+
+//people
+$peodet = array(
+					"1"=>"Biografia",
+					"2"=>"Conhecido por",
+					"3"=>"Aniversário",
+					"4"=>"Local de Nascimento",
+					"5"=>"Também Conhecido Como"
+				);
+$mlists = "Lista de Filmes de";
+
+//search
+$cari = "Resultados da busca por";
+
+//popup
+$p1 = "Ative sua conta GRATUITA!";
+$p2 = "Você deve criar uma conta para continuar assistindo";
+$p3 = "Continue a assistir gratuitamente";
+$p4 = "Registo Rápido!";
+$p5 = "Demora menos de 1 minuto para se inscrever, então você pode desfrutar de filmes ilimitados e títulos de TV.";
+
+
+
+
+
+
+
+
+
+
+?>
