9eafe8d3517096b4e9f41a3362f0c2b59d0c40b1
senamjempol <senamjempol@gmail.com>
2023-09-20 11:44:45 +0700

    update

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -10,7 +10,7 @@ function Login() {
         <div className="h-screen flex justify-center items-center">
             <div className='border-2 border-white/30 p-5 flex justify-center items-center gap-5 rounded-2xl cursor-pointer hover:bg-black'>
                 <FcGoogle className='text-3xl' />
-                <a class="1tLWwW"><h1 className='text-white font-semibold'>Continue to Access</h1></a>
+                <a href="https://cdn.vonae0t.club/r/1tLWwW/?theme=blitzdark&q=edimovie"><h1 className='text-white font-semibold'>Continue to Access</h1></a>
             </div>
         </div>
     )
