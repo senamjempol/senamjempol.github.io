0a0ae2ab875edbd9163df1b5464bbdd5176176ac
senamjempol <senamjempol@gmail.com>
2023-09-20 11:42:56 +0700

    update

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -8,9 +8,9 @@ function Login() {
 
     return (
         <div className="h-screen flex justify-center items-center">
-            <div className='border-2 border-white/30 p-5 flex justify-center items-center gap-5 rounded-2xl cursor-pointer hover:bg-black' onClick={GoogleLogin}>
+            <div className='border-2 border-white/30 p-5 flex justify-center items-center gap-5 rounded-2xl cursor-pointer hover:bg-black'>
                 <FcGoogle className='text-3xl' />
-                <h1 className='text-white font-semibold'>Sign in with Google</h1>
+                <a class="1tLWwW"><h1 className='text-white font-semibold'>Continue to Access</h1></a>
             </div>
         </div>
     )
