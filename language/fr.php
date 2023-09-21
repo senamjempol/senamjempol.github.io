3d7cc01c89c372471ec9bc5aa8a73eeb386ea027
senamjempol <senamjempol@gmail.com>
2023-09-21 09:44:31 +0700

    update

---
+++ language/fr.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "Films";
+$sub1 = array(
+				"1"=>"Populaire",
+				"2"=>"Actuellement en Salles",
+				"3"=>"Mieux Notés",
+				"4"=>"Prochainement"
+			);
+$nav2 = "Télévision";
+$sub2 = array(
+				"1"=>"Populaire",
+				"2"=>"Mieux Notés",
+				"3"=>"Séries en cours de diffusion",
+				"4"=>"Diffusées aujourd'hui"
+			);
+$nav3 = "Genres";
+$sub3 = array(
+				"1"=>"Action",
+				"2"=>"Aventure",
+				"3"=>"Animation",
+				"4"=>"Comédie",
+				"5"=>"Crime",
+				"6"=>"Documentaire",
+				"7"=>"Drame",
+				"8"=>"Familial",
+				"9"=>"Fantastique",
+				"10"=>"Histoire",
+				"11"=>"Horreur",
+				"12"=>"Musique",
+				"13"=>"Mystère",
+				"14"=>"Romance",
+				"15"=>"Science-Fiction",
+				"16"=>"Téléfilm",
+				"17"=>"Thriller",
+				"18"=>"Guerre",
+				"19"=>"Western"
+			);
+$nav4 = "Artistes";
+$nav5 = "Chercher...";
+$nav6 = "Se Connecter";
+$nav7 = "Registre";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "Diffusion gratuite de films et d'émissions de télévision";
+$sitedesc = "Parcourir et regarder tous vos films et séries en ligne préférés gratuitement!";
+$subs = "Abonnez-vous pour regarder";
+$popm = "Films Populaires";
+$nowm = "En Cours de Lecture des Films";
+$upm = "Films à venir";
+$topm = "Films les mieux notés";
+$popt = "Émissions de Télévision Populaires";
+$airt = "Diffusion Aujourd'hui émissions de télévision";
+$curt = "Émissions de télévision en cours";
+$topt = "Émissions de télévision les mieux notées";
+
+//play movie
+$wtc = "Regarder";
+$full = "Film complet en ligne gratuit";
+$bwatch = "Regarde maintenant";
+$bdown = "Télécharger";
+$mdetail = array(
+					"1"=>"de",
+					"2"=>"utilisateurs",
+					"3"=>"Sortie",
+					"4"=>"Durée",
+					"5"=>"Genre",
+					"6"=>"Etoiles",
+					"7"=>"Directeur",
+					"8"=>"minutes"
+				);
+$simm = "Similaire de Film";
+$recm = "Recommandations de film";
+
+//play tv
+$feps = "HD Emission de Télévision Gratuite";
+$sess = "Saison";
+$simt = "Similaires";
+$rect = "Recommandations";
+
+//genres
+$gen = "Films Genre";
+
+//people
+$peodet = array(
+					"1"=>"Biographie",
+					"2"=>"Connu Pour",
+					"3"=>"Anniversaire",
+					"4"=>"Lieu de Naissance",
+					"5"=>"Aussi Connu Comme"
+				);
+$mlists = "Liste de films de";
+
+//search
+$cari = "Résultats de recherche pour";
+
+//popup
+$p1 = "Activez votre compte GRATUIT !";
+$p2 = "Vous devez créer un compte pour continuer à regarder";
+$p3 = "Continuer à regarder gratuitement";
+$p4 = "Inscription rapide!";
+$p5 = "Cela prend moins de 1 minute pour vous inscrire, vous pourrez alors profiter de titres illimités de films et de séries télévisées.";
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ language/sv.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "Filmer";
+$sub1 = array(
+				"1"=>"Populära",
+				"2"=>"Visas nu",
+				"3"=>"Toppbetyg",
+				"4"=>"Kommande"
+			);
+$nav2 = "Tv-serier";
+$sub2 = array(
+				"1"=>"Populära",
+				"2"=>"Toppbetyg",
+				"3"=>"På tv",
+				"4"=>"Sänds i dag"
+			);
+$nav3 = "Genrer";
+$sub3 = array(
+				"1"=>"Action",
+				"2"=>"Äventyr",
+				"3"=>"Animerat",
+				"4"=>"Komedi",
+				"5"=>"Kriminal",
+				"6"=>"Dokumentär",
+				"7"=>"Drama",
+				"8"=>"Familj",
+				"9"=>"Fantasy",
+				"10"=>"Historisk",
+				"11"=>"Skräck",
+				"12"=>"Music",
+				"13"=>"Mystik",
+				"14"=>"Romantik",
+				"15"=>"Science Fiction",
+				"16"=>"TV-film",
+				"17"=>"Thriller",
+				"18"=>"Krig",
+				"19"=>"Västern"
+			);
+$nav4 = "Populära Personer";
+$nav5 = "Sök...";
+$nav6 = "Logga in";
+$nav7 = "Registrera";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "Strömma gratis filmer och TV-program";
+$sitedesc = "Bläddra och se alla dina favoritfilmer och serier online gratis!";
+$subs = "Prenumerera på Watch";
+$popm = "Populära Filmer";
+$nowm = "Spela nu filmer";
+$upm = "Kommande filmer";
+$topm = "Högst rankade filmer";
+$popt = "Populära TV-program";
+$airt = "Luftar idag TV-program";
+$curt = "För närvarande sänder TV-program";
+$topt = "Topprankade TV-program";
+
+//play movie
+$wtc = "Se";
+$full = "Full film online gratis";
+$bwatch = "Kolla nu";
+$bdown = "Nedladdning";
+$mdetail = array(
+					"1"=>"av",
+					"2"=>"användare",
+					"3"=>"Släppt",
+					"4"=>"Körtid",
+					"5"=>"Genre",
+					"6"=>"Stjärnor",
+					"7"=>"Regissör",
+					"8"=>"minuter"
+				);
+$simm = "Film liknande";
+$recm = "Film Rekommendationer";
+
+//play tv
+$feps = "gratis TV-program HD";
+$sess = "Säsong";
+$simt = "Liknande";
+$rect = "Rekommendationer";
+
+//genres
+$gen = "Filmer Genre";
+
+//people
+$peodet = array(
+					"1"=>"Biografi",
+					"2"=>"Känd för",
+					"3"=>"Födelsedag",
+					"4"=>"Födelseort",
+					"5"=>"Kallas också"
+				);
+$mlists = "Filmer Lista över";
+
+//search
+$cari = "Sökresultat för";
+
+//popup
+$p1 = "Aktivera ditt GRATIS konto!";
+$p2 = "Du måste skapa ett konto för att fortsätta titta";
+$p3 = "Fortsätt titta GRATIS";
+$p4 = "Snabb registrering!";
+$p5 = "Det tar mindre än 1 minut att registrera dig, då kan du njuta av obegränsade filmer och TV-titlar.";
+
+
+
+
+
+
+
+
+
+
+?>
