6cc985d829ce9b3d768c9be4dedae6165221fed7
senamjempol <senamjempol@gmail.com>
2023-09-23 08:16:32 +0700

    update

---
+++ language/ko.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "영화";
+$sub1 = array(
+				"1"=>"인기",
+				"2"=>"현재 상영 중",
+				"3"=>"높은 평점",
+				"4"=>"개봉 예정"
+			);
+$nav2 = "TV 프로그램";
+$sub2 = array(
+				"1"=>"인기",
+				"2"=>"높은 평점",
+				"3"=>"TV 방영 중",
+				"4"=>"오늘 방영"
+			);
+$nav3 = "장르";
+$sub3 = array(
+				"1"=>"액션",
+				"2"=>"모험",
+				"3"=>"애니메이션",
+				"4"=>"코미디",
+				"5"=>"범죄",
+				"6"=>"다큐멘터리",
+				"7"=>"드라마",
+				"8"=>"가족",
+				"9"=>"판타지",
+				"10"=>"역사",
+				"11"=>"공포",
+				"12"=>"음악",
+				"13"=>"미스터리",
+				"14"=>"로맨스",
+				"15"=>"SF",
+				"16"=>"TV 영화",
+				"17"=>"스릴러",
+				"18"=>"전쟁",
+				"19"=>"서부"
+			);
+$nav4 = "인기있는 사람들";
+$nav5 = "수색...";
+$nav6 = "로그인";
+$nav7 = "레지스터";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "무료 영화 및 TV 프로그램 스트림";
+$sitedesc = "좋아하는 온라인 영화 및 시리즈를 무료로 찾아보고보십시오!";
+$subs = "시계 구독 신청";
+$popm = "인기 영화";
+$nowm = "지금 영화 재생 중";
+$upm = "다가오는 영화";
+$topm = "최고 평점 영화";
+$popt = "인기 TV 쇼";
+$airt = "오늘 방송하는 TV 쇼";
+$curt = "현재 방송중인 TV 프로그램";
+$topt = "최고 평점 TV 쇼";
+
+//play movie
+$wtc = "시계";
+$full = "전체 영화 온라인 무료";
+$bwatch = "지금보기";
+$bdown = "다운로드";
+$mdetail = array(
+					"1"=>"으로",
+					"2"=>"사용자",
+					"3"=>"출시 됨",
+					"4"=>"실행 시간",
+					"5"=>"유형",
+					"6"=>"별",
+					"7"=>"감독",
+					"8"=>"의사록"
+				);
+$simm = "비슷한 영화";
+$recm = "영화 추천";
+
+//play tv
+$feps = "HD 무료 TV 쇼보기";
+$sess = "시즌";
+$simt = "비슷한";
+$rect = "권장 사항";
+
+//genres
+$gen = "영화 장르";
+
+//people
+$peodet = array(
+					"1"=>"전기",
+					"2"=>"알려진 대상",
+					"3"=>"생일",
+					"4"=>"출생지",
+					"5"=>"또한 ~으로 알려진"
+				);
+$mlists = "";
+
+//search
+$cari = "에 대한 검색 결과";
+
+//popup
+$p1 = "무료 계정을 활성화하십시오!";
+$p2 = "계속 시청하려면 계정을 만들어야합니다.";
+$p3 = "무료로 계속 시청하십시오.";
+$p4 = "빠른 가입!";
+$p5 = "가입하는 데 1 분이 채되지 않아 무제한 영화 및 TV 타이틀을 즐길 수 있습니다.";
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ language/tr.php
@@ -0,0 +1,132 @@
+<?php
+//navbar
+$nav1 = "Filmler";
+$sub1 = array(
+				"1"=>"Popüler",
+				"2"=>"Gösterimde",
+				"3"=>"En Fazla Oy Alanlar",
+				"4"=>"Gelecek"
+			);
+$nav2 = "Diziler";
+$sub2 = array(
+				"1"=>"Popüler",
+				"2"=>"En Fazla Oy Alanlar",
+				"3"=>"Televizyonda",
+				"4"=>"Günün Programı"
+			);
+$nav3 = "Türler";
+$sub3 = array(
+				"1"=>"Aksiyon",
+				"2"=>"Macera",
+				"3"=>"Animasyon",
+				"4"=>"Komedi",
+				"5"=>"Suç",
+				"6"=>"Belgesel",
+				"7"=>"Dram",
+				"8"=>"Aile",
+				"9"=>"Fantastik",
+				"10"=>"Tarih",
+				"11"=>"Korku",
+				"12"=>"Müzik",
+				"13"=>"Gizem",
+				"14"=>"Romantik",
+				"15"=>"Bilim-Kurgu",
+				"16"=>"TV film",
+				"17"=>"Gerilim",
+				"18"=>"Savaş",
+				"19"=>"Vahşi Batı"
+			);
+$nav4 = "Popüler İnsanlar";
+$nav5 = "Arama...";
+$nav6 = "Oturum aç";
+$nav7 = "Kayıt olmak";
+$signin = array(
+				"1"=>"Email",
+				"2"=>"Enter Email",
+				"3"=>"We'll never share your email with anyone else.",
+				"4"=>"Password",
+				"5"=>"Forgot Password?",
+				"6"=>"Or",
+				"7"=>"Create Free Account",
+				"8"=>"Loading...",
+				"9"=>"Oops! You forgot to enter an email address",
+				"10"=>"Uh oh! The email address is invalid",
+				"11"=>"Oops! You forgot to enter a password",
+				"12"=>"Oops! No account was found with that email, please try again or",
+				"13"=>"Create an account",
+				"14"=>"Reset Password",
+				"15"=>"Enter your email address and we'll send you a link to reset your password.",
+				"16"=>"Back to Sign In"
+			);
+$setlang = "Choose your language:";			
+
+//index
+$sitetitle = "Stream Ücretsiz Filmler ve TV Şovları";
+$sitedesc = "Gözatın ve en sevdiğiniz tüm çevrimiçi filmleri ve dizileri ücretsiz izleyin!";
+$subs = "İzlemeye Abone Ol";
+$popm = "Popüler Filmler";
+$nowm = "Şimdi Filmler Oynuyor";
+$upm = "Gelecek filmler";
+$topm = "En İyi Filmler";
+$popt = "Popüler TV Şovları";
+$airt = "Bugün Yayında TV Şovları";
+$curt = "Şu Anda Canlı TV Şovları";
+$topt = "En Çok Puan Alan TV Şovları";
+
+//play movie
+$wtc = "";
+$full = "Bedava Film İzle Full Streaming Online";
+$bwatch = "İzle şimdi";
+$bdown = "İndir";
+$mdetail = array(
+					"1"=>"tarafından",
+					"2"=>"kullanıcı",
+					"3"=>"Çıkış",
+					"4"=>"Çalışma Süresi",
+					"5"=>"Tür",
+					"6"=>"Yıldızlar",
+					"7"=>"Yönetmen",
+					"8"=>"dakika"
+				);
+$simm = "Film benzerleri";
+$recm = "Film Önerileri";
+
+//play tv
+$feps = "HD Ücretsiz TV Şovunu İzleyin";
+$sess = "Sezon";
+$simt = "Benzer";
+$rect = "öneriler";
+
+//genres
+$gen = "Filmler Tür";
+
+//people
+$peodet = array(
+					"1"=>"Biyografi",
+					"2"=>"Bilinen",
+					"3"=>"Doğum Günü",
+					"4"=>"Doğum Yeri",
+					"5"=>"Diğer Adı"
+				);
+$mlists = "Film Listesi";
+
+//search
+$cari = "için arama sonuçları";
+
+//popup
+$p1 = "ÜCRETSİZ Hesabınızı etkinleştirin!";
+$p2 = "İzlemeye devam etmek için bir hesap oluşturmanız gerekir";
+$p3 = "ÜCRETSİZ izlemeye devam edin";
+$p4 = "Hızlı Kayıt!";
+$p5 = "Kayıt olmak 1 dakikadan az sürüyor, ardından Sınırsız Film ve TV başlıklarının keyfini çıkarabilirsiniz.";
+
+
+
+
+
+
+
+
+
+
+?>
